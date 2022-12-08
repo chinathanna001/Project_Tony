e59374d8edea05c655c7693c1058ca28dc67003e
chinathanna001 <90709670+chinathanna001@users.noreply.github.com>
2022-12-08 11:11:22 +0530

    .

---
+++ Data Science/Linear Regression.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import statsmodels.api as sm
+dataset = pd.read_csv('Salary_Data.csv')
+print("First five lines",dataset.head())
+X = dataset.iloc[:,:-1].values #independent variable array
+y = dataset.iloc[:,1].values #dependent variable vector
+lmregmodel = sm.OLS(y,X)
+result = lmregmodel.fit()
+print(result.summary())
